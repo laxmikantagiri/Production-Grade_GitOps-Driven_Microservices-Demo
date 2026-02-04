4a3ae0887c652b9983dafb38f7da3fcda9532d08
lax66 <laxmikantamlucky@gmail.com>
2026-02-04 13:02:06 +0000

    add tag to subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -17,4 +17,10 @@ module "vpc" {
     Terraform = "true"
     Environment = "dev"
   }
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
 }
